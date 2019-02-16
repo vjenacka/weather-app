4d206f465b5cbe131b7da432a6658a2490157480
vjenacka <edins613@gmail.com>
2019-02-17 00:10:59 +0100

    Finished with the hourly forcast

--- src/App.js
+++ src/App.js
@@ -3,71 +3,85 @@ import Collection from './components/Collection/Collection';
 
 
 /* STUFF TO DO 
--split the code in to multiple functions
--try removing the full date and just make the time show
 -check for bugs
 -fetch the 5 day data
 */
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {
-      forcast:[
-        { id: 1, day: 'Mon', highestTemp: 20, lowestTemp: 10 , active: true, hours:[11,33,40,14,15]},
-        { id: 2, day: 'Tue', highestTemp: 30, lowestTemp: 20, active: false, hours:[12,33,40,14,15] },
-        { id: 3, day: 'Wed', highestTemp: 40, lowestTemp: 30, active: false, hours:[13,33,40,14,15]},
-        { id: 4, day: 'Thu', highestTemp: 50, lowestTemp: 40, active: false, hours:[14,33,40,14,15] },
-        { id: 5, day: 'Fri', highestTemp: 60, lowestTemp: 50, active: false, hours:[15,33,40,14,15] }
-      ],
-      data: 'dummy data'
+    this.state = {
+      forcast: [
+        { id: 1, date: 17, day: 'Sun', highestTemp: 20, lowestTemp: 10, active: true, hours: [] },
+        { id: 2, date: 18, day: 'Mon', highestTemp: 30, lowestTemp: 20, active: false, hours: [] },
+        { id: 3, date: 19, day: 'Tue', highestTemp: 40, lowestTemp: 30, active: false, hours: [] },
+        { id: 4, date: 20, day: 'Wed', highestTemp: 50, lowestTemp: 40, active: false, hours: [] },
+        { id: 5, date: 21, day: 'Thu', highestTemp: 60, lowestTemp: 50, active: false, hours: [] }
+      ]
     }
   }
   //call the api and get 5 day/3 hour forcast
-  componentDidMount(){
-    const API_KEY= 'a68987c5b03a0c8ce52ac5e15514eb33';
+  componentDidMount() {
+    const API_KEY = 'a68987c5b03a0c8ce52ac5e15514eb33';
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Zenica,ba&units=metric&appid=${API_KEY}`)
-    .then(response =>{
-      return response.json()
-    })
-    .then(JSONData =>{
-      console.log(JSONData);
-      //goes through the list and gets the date and the temp of the current time
-      const formatedData = JSONData.list.map(item =>{
-        return {
-          day: this.dayConverter(item.dt),
-          hour: this.hourConverter(item.dt),
-          temp: item.main.temp
-        }
-      });
-      console.log(formatedData);
-      const wtf = this.state.forcast.map(ele =>{
-        ele.hours=formatedData.slice(0,7);
-        return ele;
+      .then(response => {
+        return response.json()
       })
-      this.setState({
-        forcast:wtf
+      .then(JSONData => {
+        const formatedData= this.formatData(JSONData);
+        
+        this.setState({
+          forcast: this.storeHours(this.state.forcast, formatedData)
+        })
       })
-    })
   }
-  ///format data function
-  hourConverter=(UNIX_timestamp) => {
-    var a = new Date(UNIX_timestamp * 1000);
-    return a.getHours();
-  }
-  dayConverter=(UNIX_timestamp) => {
-    var a = new Date(UNIX_timestamp * 1000);
-    return a.getDate();
-  }
-  //test function
-  /* test = ()=>{
 
-  } */
+  //goes through the JSONData and extracts the day of the forcast, hour and temp of that hour
+  formatData= data =>{
+    return data.list.map(item => {
+      return {
+        day: this.dayConverter(item.dt),
+        hour: this.hourConverter(item.dt),
+        temp: Math.floor(item.main.temp)
+      }
+    });
+  }
+
+  //goes through the state.forcast array and the hourcast array and returns an updated forcast array
+  storeHours = (forecast, hourcast) => {
+    const newForcast = forecast.map(ele => {
+      let arr = [];
+      hourcast.forEach(hr => {
+        let obj = {
+          hour: null,
+          temp: null
+        };
+        //stores the hour and temp in an object
+        if (hr.day === ele.date && hr.day != null) {
+          obj.hour = hr.hour;
+          obj.temp = hr.temp;
+        }
+        //removes the not used objects
+        if (obj.hour != null && obj.temp != null) {
+          arr.push(obj);
+        }
+      })
+      //stores the array of hourcast objects in the individual forcast.hours
+      ele.hours=arr;
+      return ele;
+    })
+    return newForcast;
+  }
+
+  ///format the forcast date
+  hourConverter = UNIX_timestamp => new Date(UNIX_timestamp * 1000).getHours();
+  dayConverter = (UNIX_timestamp) => new Date(UNIX_timestamp * 1000).getDate();
+ 
   //changes the active state of the component that renders the hourly forcast
-  hourlyForcast= id =>{
-    const newForcast= this.state.forcast.map( day =>{
-      if(day.id === id){
+  hourlyForcast = id => {
+    const newForcast = this.state.forcast.map(day => {
+      if (day.id === id) {
         day.active = !day.active;
       }
       return day;
@@ -80,7 +94,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Collection forcast={this.state.forcast} hourlyForcast={this.hourlyForcast}/>
+        <Collection forcast={this.state.forcast} hourlyForcast={this.hourlyForcast} />
       </div>
     );
   }
--- src/components/HourlyForcast/HourlyForcast.js
+++ src/components/HourlyForcast/HourlyForcast.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './HourlyForcast.css';
 
 export default function HourlyForcast({ active, hours }) {
-    const hourlyDiv = hours.map((hour) => {
-        return <li key={hour.date}>{hour.date} :{hour.temp}&deg;</li>
+    const hourlyDiv = hours.map( hr => {
+        return <li key={Math.floor(Math.random()*5000)}>{hr.hour} h: {hr.temp}&deg;</li>
     });
     const renderActive = active ? (
         <div className='hourly-forcast'>
